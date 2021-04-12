dcd1279569beaa5c346e3b9c470423a4d0830f19
Mateus Guimarães Alves <guimaraesalves45@gmail.com>
2021-04-12 18:54:08 -0300

    Aulas rocketseat
    Estudo e exercícios da plataforma rocketseat

---
+++ Rocketseat/JS/function.js
@@ -0,0 +1,15 @@
+// criar um aplicativo de frases motivacionais
+console.log('Estudar é muito bom')
+console.log('Paciência e persistência')
+console.log('Revisão é mãe')
+
+
+// criar um aplicativo de frases motivacionais
+console.log('Estudar é muito bom')
+console.log('Paciência e persistência')
+console.log('Revisão é mãe')
+
+// criar um aplicativo de frases motivacionais
+console.log('Estudar é muito bom')
+console.log('Paciência e persistência')
+console.log('Revisão é mãe')
