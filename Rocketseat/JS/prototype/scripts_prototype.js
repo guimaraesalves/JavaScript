e311da85c4a0faf8d378a104158c5e87102e0729
Mateus Guimarães Alves <guimaraesalves45@gmail.com>
2021-04-13 19:30:03 -0300

    Update scripts_prototype.js

--- Rocketseat/JS/prototype/scripts_prototype.js
+++ Rocketseat/JS/prototype/scripts_prototype.js
@@ -9,3 +9,32 @@
 
 // tipo biblioteca em python
 
+//__proto__ é as propriedades que já existem no javascript
+
+/*
+
+Type conversion (typecasting) vs Type coersion
+
+* Alteração de um tipo de dado para outro tipo
+*/
+
+console.log('9' + 5) //transformou o int 5 em str
+
+console.log(Number('9') + 5) //typecasting str "9" para número
+
+
+// Manipulando Strings e Números
+
+// Transformar String em Número e Número em string
+let string = '123'
+console.log(Number(string))
+let number = 321
+console.log(String(typeof number))
+
+
+// Contar quantos caracteres tem uma palavra e quantos digitos tem um número
+let word = "paralelepipido"
+console.log(word.length)
+let num = 1234
+console.log(String(num).length)
+
