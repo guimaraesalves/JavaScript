bcf68dad8d443b772cb8fa7a27ca96dceaee4601
Mateus Guimarães Alves <guimaraesalves45@gmail.com>
2021-05-17 10:57:43 -0300

    commit aula style

---
+++ Rocketseat/PreparaçãodeAstronautas/Style/classList.js
@@ -0,0 +1,7 @@
+// Alterando estilos
+// classList
+const element = document.querySelector('body')
+
+element.classList.add('activate', 'green')
+console.log(element.classList)
+element.classList.remove('active')
---
+++ Rocketseat/PreparaçãodeAstronautas/Style/script.js
@@ -0,0 +1,6 @@
+// Alterando estilos
+const element = document.querySelector('body')
+
+element.style.backgroundColor = "#346399"
+// pegar o elemento
+console.log(element.style.backgroundColor);
